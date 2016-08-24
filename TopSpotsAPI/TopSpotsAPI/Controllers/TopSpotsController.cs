94952ff5a1753efa985c7580d09110e07cf0612d
Jason Land <Jason Land>
2016-08-24 15:28:58 -0700

    added json parser, json file, cors handling. project works now

--- TopSpotsAPI/TopSpotsAPI/Controllers/TopSpotsController.cs
+++ TopSpotsAPI/TopSpotsAPI/Controllers/TopSpotsController.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.IO;
 using System.Net.Http;
 using System.Web.Http;
+using Newtonsoft.Json;
+using TopSpotsAPI.Models;
 
 namespace TopSpotsAPI.Controllers
 {
     public class TopSpotsController : ApiController
     {
+        //read /api/TopSpots
+        public IEnumerable<TopSpot> Get()
+        {
+            // 1. get/read file into a string and deserialize JSON to a type
+            TopSpot[] topspots = JsonConvert.DeserializeObject<TopSpot[]>(File.ReadAllText(@"C:\Users\wishi\Documents\OCA\13-TopSpotsAPI\TopSpotsAPI\TopSpotsAPI\topspots.json"));
+            return topspots;
+        }
+
+        //create
+        public TopSpotsAPI Post()
+        {
+
+        }
+
+        //update
+
+        //delete
+
+        public void Delete()
+        {
+
+        }
     }
 }
